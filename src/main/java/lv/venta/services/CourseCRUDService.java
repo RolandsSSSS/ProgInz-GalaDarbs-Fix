36f4838a043986a9943405fe1efdd1f6c24a23f7
RolandsSSSS <s20saforola@venta.lv>
2023-06-30 00:11:28 +0300

    Add: Pievienoju controllers Home un Course, kā arī Course service ar impl

---
+++ src/main/java/lv/venta/services/CourseCRUDService.java
@@ -0,0 +1,27 @@
+package lv.venta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lv.venta.models.Course;
+import lv.venta.repos.ICourseRepo;
+
+@Service
+public class CourseCRUDService implements ICourseCRUDService{
+
+	@Autowired
+	private ICourseRepo courseRepo;
+	
+	@Autowired
+	public CourseCRUDService(ICourseRepo courseRepo) {
+		this.courseRepo = courseRepo;
+	}
+
+	@Override
+	public List<Course> selectAllCourses() {
+		return (List<Course>)courseRepo.findAll();
+	}
+	
+}
