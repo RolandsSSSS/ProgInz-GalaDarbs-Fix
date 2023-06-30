852da3b1d8b2b6fb619d9a718456e85015d14276
RolandsSSSS <s20saforola@venta.lv>
2023-06-30 04:29:49 +0300

    Add: Pievienoju jaunu course update page, pievienoju delete by id, update by id

--- src/main/java/lv/venta/services/impl/ICourseCRUDService.java
+++ src/main/java/lv/venta/services/impl/ICourseCRUDService.java
@@ -7,7 +7,10 @@ import lv.venta.models.Course;
 public interface ICourseCRUDService {
 
 	List<Course> selectAllCourses();
-	
+
 	Course selectCourseById(long idc);
 
+	void deleteCourseById(long idc);
+
+	void updateCourseById(long idc, Course updatedCourse);
 }
