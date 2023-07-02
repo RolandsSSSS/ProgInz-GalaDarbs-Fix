d83e3042c4224963a4e3992b3e784d6940f8ce14
RolandsSSSS <s20saforola@venta.lv>
2023-07-02 04:49:19 +0300

    Add: Pievienoju show by id

--- src/main/java/lv/venta/controllers/ThesisController.java
+++ src/main/java/lv/venta/controllers/ThesisController.java
@@ -3,6 +3,7 @@ package lv.venta.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lv.venta.services.ThesisCRUDService;
@@ -18,5 +19,11 @@ public class ThesisController {
 	public String selectAllThesis(org.springframework.ui.Model thesis) {
 		thesis.addAttribute("MyThesis", thesisService.selectAllThesis());
 		return "thesis-all-page";
+	}
+	
+	@GetMapping("/showAll/{id}")
+	public String selectThesisById(@PathVariable long id, org.springframework.ui.Model thesis) {
+		thesis.addAttribute("MyThesis", thesisService.selectThesisById(id));
+		return "thesis-one-page";
 	}
 }
