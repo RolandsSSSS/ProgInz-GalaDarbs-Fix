82a4c1566e03656bcd32ea544a8404875be1e977
RolandsSSSS <s20saforola@venta.lv>
2024-01-24 16:43:49 +0200

    Fix: Salaboju code smells controllieri priekš Person

--- src/main/java/lv/venta/controllers/PersonsController.java
+++ src/main/java/lv/venta/controllers/PersonsController.java
@@ -1,9 +1,6 @@
 package lv.venta.controllers;
 
 import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,121 +12,109 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
 import lv.venta.services.IPersonCRUD;
-import lv.venta.services.IPersonCRUDService;
-import lv.venta.models.Course;
 import lv.venta.models.users.AcademicPersonel;
 import lv.venta.models.users.Person;
-import lv.venta.models.users.Student;
 import lv.venta.repos.IPersonRepo;
 
 @Controller
 @RequestMapping("/Person")
 public class PersonsController {
-	
-	@Autowired
+	public static final String PERSON_ATTRIBUTE = "AllPersons";
+	private static final String REDIRECT_TO_SHOW_ALL = "redirect:/Person/All";
+
 	private IPersonRepo personRepo;
-	@Autowired
 	private IPersonCRUD personCrud;
-	
-	
+
+	public PersonsController(IPersonRepo personRepo, IPersonCRUD personCrud) {
+		this.personRepo = personRepo;
+		this.personCrud = personCrud;
+	}
 
 	@GetMapping("/All")
 	public String selectAllPersons(Model model) {
 		List<Person> allPersons = (List<Person>) personRepo.findAll();
-		model.addAttribute("AllPersons", allPersons);
+		model.addAttribute(PERSON_ATTRIBUTE, allPersons);
 		return "Persons-All";
 	}
+
 	@GetMapping("/All/{id}")
 	public String selectCourseById(@PathVariable long id, Model model) {
-	    try {
-	  
-	        model.addAttribute("AllPersons", personCrud.retrieveOnePersonById(id));
-	        return "Persons-One"; 
-	    } catch (Exception e) {
-	        
-	        return "error-page";
-	    }
+		try {
+
+			model.addAttribute(PERSON_ATTRIBUTE, personCrud.retrieveOnePersonById(id));
+			return "Persons-One";
+		} catch (Exception e) {
+
+			return "error-page";
+		}
 	}
-	
+
 	@GetMapping("/update/{id}")
-	public String UdatePerson(@PathVariable long id, Model model) throws Exception {
-	  
-	    Person person = personCrud.retrieveOnePersonById(id);
+	public String UpdatePerson(@PathVariable long id, Model model) throws Exception {
 
+		Person person = personCrud.retrieveOnePersonById(id);
 
-	    model.addAttribute("person", person);
+		model.addAttribute("person", person);
 
-	    return "Person-Update";
+		return "Person-Update";
 	}
 
 	@PostMapping("/update/By/{id}")
 	public String updatePersonById(@PathVariable long id, @ModelAttribute("person") @Valid Person updatedPerson,
-	        BindingResult bindingResult, Model model) throws Exception {
-	    if (bindingResult.hasErrors()) {
-	        return "Person-Update";
-	    }
+			BindingResult bindingResult, Model model) throws Exception {
+		if (bindingResult.hasErrors()) {
+			return "Person-Update";
+		}
 
-	   
-	    Person existingPerson = personCrud.retrieveOnePersonById(id);
-	    existingPerson.setName(updatedPerson.getName());
-	    existingPerson.setSurname(updatedPerson.getSurname());
-	    existingPerson.setPersoncode(updatedPerson.getPersoncode());
-	    personCrud.updatePersonByParams(existingPerson);
+		Person existingPerson = personCrud.retrieveOnePersonById(id);
+		existingPerson.setName(updatedPerson.getName());
+		existingPerson.setSurname(updatedPerson.getSurname());
+		existingPerson.setPersoncode(updatedPerson.getPersoncode());
+		personCrud.updatePersonByParams(existingPerson);
 
-	    return "redirect:/Person/All";
+		return REDIRECT_TO_SHOW_ALL;
 	}
-
-
-	
-	
 
 	@PostMapping("remove/{id}")
 	public String deletePersonById(@PathVariable("id") long id, Model model) throws Exception {
-	    try {
-	    	 if ( personCrud.retrieveOnePersonById(id) instanceof AcademicPersonel) {
-	    		 return "redirect:/academicPersonel/remove/{id}";
-	            } 
-	    	/* 	else if (personCrud.retrieveOnePersonById(id) instanceof Student) {
-	    	 		return "redirect:/academicPersonel/remove/{id}";
-				}*/
-	    	 else {
-	            	 personCrud.deletePersonById(id);
-	     	        model.addAttribute("AllPersons", personCrud.selectAllPersons());
-	     	        return "redirect:/Person/All";
-	            }
-	           
-	        
-	    } catch (Exception e) {
-	        return "error-page";
-	    }
+		try {
+			if (personCrud.retrieveOnePersonById(id) instanceof AcademicPersonel) {
+				return "redirect:/academicPersonel/remove/{id}";
+			} else {
+				personCrud.deletePersonById(id);
+				model.addAttribute(PERSON_ATTRIBUTE, personCrud.selectAllPersons());
+				return REDIRECT_TO_SHOW_ALL;
+			}
+
+		} catch (Exception e) {
+			return "error-page";
+		}
 	}
 
+	@GetMapping("/AddPage")
+	public String addNewPerson(Model model) {
 
+		return "Person-Add";
+	}
 
-			@GetMapping("/AddPage")
-					public String AddNewPerson(Model model) {
-				
-					return "Person-Add";
-}
-			@PostMapping("/Add")
-			public String addNewPerson(@ModelAttribute("personForm") @Valid Person person, BindingResult bindingResult) {
-			    if (bindingResult.hasErrors()) {
-			        return "Person-Add";
-			    }
-			    
-			    Person temp = new Person();
-			    temp.setName(person.getName());
-			    temp.setSurname(person.getSurname());
-			    temp.setPersoncode(person.getPersoncode());
-			    
-			    try {
-			    	personCrud.insertPersontByParams(person);
-			    } catch (Exception e) {
-			 
-			    }
+	@PostMapping("/Add")
+	public String addNewPerson2(@ModelAttribute("personForm") @Valid Person person, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "Person-Add";
+		}
 
-			    return "redirect:/Person/All";
-			}
-		
+		Person temp = new Person();
+		temp.setName(person.getName());
+		temp.setSurname(person.getSurname());
+		temp.setPersoncode(person.getPersoncode());
+
+		try {
+			personCrud.insertPersontByParams(person);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return REDIRECT_TO_SHOW_ALL;
+	}
 
 }
